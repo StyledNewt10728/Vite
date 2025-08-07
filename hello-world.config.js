ea1a19ec2ba8ecf254f1e8879e10d4a87ed33a99
StyledNewt10728 <118925158+StyledNewt10728@users.noreply.github.com>
2025-08-07 13:02:33 -0400

    Add files via upload

---
+++ hello-world.config.js
@@ -0,0 +1,9 @@
+//I don't know if I did this right, I included a screen shot to the project submission of me doing the steps in terminal as well as these file within github.
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
